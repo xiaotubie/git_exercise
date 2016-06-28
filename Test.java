ea01b6f6d2557e4f99395092732588ca3001ec4d
xiaotubie <brucegao.gz@gmail.com>
2016-06-28 16:16:27 +0800

    multi

--- Test.java
+++ Test.java
@@ -5,6 +5,11 @@ public class Test {
    public static void main(String[] args) {
 
        System.out.println("hello,world");
+       System.out.println("hello,world");
+
+       System.out.println("hello,world");
+       System.out.println("hello,world");
+       System.out.println("hello,world");
 
    }
 
